7d9445ec4d1beddeebee7e94199b72a72d53a12f
Raihan <raihan@its-macbook-pro.local>
2019-01-30 19:07:31 +0600

    initial Commit

--- Networking/ViewController.swift
+++ Networking/ViewController.swift
@@ -10,9 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageview: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    let sharedSession = URLSession.shared
+   
+        if let url = URL(string: "https://goo.gl/wV9G4I"){
+            // create reuest
+            let request = URLRequest(url: url)
+        
+            // create Data Task
+            
+            let dataTask = sharedSession.dataTask(with: request, completionHandler:{ (data, response, error) -> Void in
+                
+                if let data = data, let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self.imageview.image = image
+                    }
+                }
+            })
+            
+            dataTask.resume()
+        
+        }
     }
 
     override func didReceiveMemoryWarning() {
